284546c70458584a1e8f5cf2d79b8536a423169a
Bhavesh <bhaveshrajput9682@gmail.com>
2023-09-15 19:18:09 +0530

    Add

---
+++ DAY3/defaultArg.py
@@ -0,0 +1,8 @@
+def function1 (a, b=10):
+    print("value of a: ", a)
+    print("value of b: ", b)
+    return 
+
+function1(a="Bhavesh")
+function1(a="suresh", b=30)
+
---
+++ DAY3/list.py
@@ -0,0 +1,13 @@
+ages = [19,26,22]
+print(ages)
+
+list2 = ["bhavesh","suresh"]
+print(list2)
+
+list3 = []
+print(list3)
+
+list4 = ["python", "C", "C++"]
+print(list4[2])
+print(list4[:2])
+print(list4[:])
