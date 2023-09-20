a0e75fa8f4ecb3aef29788843a4b3e7600a8b5b5
Bhavesh <bhaveshrajput9682@gmail.com>
2023-09-20 14:06:58 +0530

    update

---
+++ DAY5/factorialModule.py
@@ -0,0 +1,7 @@
+import math
+     
+x = int(input("enter a number: "))
+     
+# returning the factorial
+print ("The factorial of 5 is : ", end ="")
+print (math.factorial(x))
--- third.py
+++ third.py
@@ -1,15 +1,14 @@
-# n = int(input("enter a No."))
-# for i in range(1,n+1):
-#   print(i)
+n = int(input("enter a No."))
+for i in range(1,n+1):
+  print(i)
 
-## sum of all NO
-# n = int(input("enter a no"))
-# sum = 0
-# for i in range(1,n+1):
-#  sum = sum + i
-#  print("The total no. of sum is:",sum)
+# sum of all NO
+n = int(input("enter a no"))
+sum = 0
+for i in range(1,n+1):
+ sum = sum + i
+ print("The total no. of sum is:",sum)
 
-#Factorial
 
 
 
